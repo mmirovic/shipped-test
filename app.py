6c0cdc3fe8c1e87fe1a8c814b011509caf032782
mmirovic <mmirovic@gmail.com>
2016-09-08 14:41:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!!"
+    # return "Hello World!!"
+    return render_template('hello.html')
+    
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
 
 
 if __name__ == "__main__":
